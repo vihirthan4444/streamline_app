23c531131a76d46fd3588ca471c1db49146b8f74
vihirthan <vihirthan@eart.lk>
2026-01-30 15:47:02 +0530

    cd: enable release apk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,11 +30,19 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            // Usually put the keystore in the 'android' directory (parent of 'app')
+            storeFile = file("../release.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
